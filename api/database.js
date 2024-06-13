29c2a9af94cfb381696f8e056418cccff4b9d746
YOA\yoaas <syogaadi75@gmail.com>
2024-06-13 20:31:40 +0700

    ubah

--- api/database.js
+++ api/database.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize')
+const mysql2 = require('mysql2')
 
 // Konfigurasi database
 const sequelize = new Sequelize('sql12713648', 'sql12713648', 'Xd3tyGDpdt', {
   host: 'sql12.freemysqlhosting.net', // Ganti dengan hostname database
   dialect: 'mysql',
+  dialectModule: mysql2,
   logging: false // Set to true untuk debug SQL queries
 })
 
